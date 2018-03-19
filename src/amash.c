e75ed6522bdd9f8ec10e992a7bcb030606157ad6
Sumeet Padavala <sumeet.padavala@gmail.com>
2018-03-19 11:06:40 +0530

    Run beautify

--- src/amash.c
+++ src/amash.c
@@ -1,19 +1,19 @@
 /*!
    \file amash.c
    \brief Contains implementations for core functions.
-*/
+ */
 
 #include "amash.h"
 #include <string.h>
 
-void strip_input(char * input)
+void strip_input(char *input)
 {
-        input[strlen(input)-1] = '\0';
+    input[strlen(input) - 1] = '\0';
 }
 
 bool startsWith(const char *pre, const char *str)
 {
-        return strncmp(pre, str, strlen(pre)) == 0;
+    return strncmp(pre, str, strlen(pre)) == 0;
 }
 
 void printPrompt()
@@ -21,44 +21,46 @@ void printPrompt()
     char cwd[100];
     char login[100];
     char hostname[100];
+
     getcwd(cwd, sizeof(cwd));
     getlogin_r(login, sizeof(login));
     gethostname(hostname, sizeof(hostname));
-    printf(KBLU "\n%s@%s",hostname,login);
-    printf(KRED":");
-    printf(KWHT"%s",cwd);
-    printf(KRED"$");
+    printf(KBLU "\n%s@%s", hostname, login);
+    printf(KRED ":");
+    printf(KWHT "%s", cwd);
+    printf(KRED "$");
     printf(KGRN "");
 }
 
 int run_event_loop()
 {
     char input[100];
-        while(1)
-        {
-                printPrompt();
-                fgets(input, 1000, stdin);
-                strip_input(input);
 
-                log_debug("input : <%s>",input);
+    while (1)
+    {
+        printPrompt();
+        fgets(input, 1000, stdin);
+        strip_input(input);
 
-                Executable* e = parse_single(input);
-                exec_program(e);
+        log_debug("input : <%s>", input);
 
-        }
-        return 0;
+        Executable *e = parse_single(input);
+        exec_program(e);
+    }
+    return 0;
 }
 
 //TODO add something cool here
-/*
-void print_intro_screen()
-{
 
-}
-*/
+/*
+   void print_intro_screen()
+   {
+
+   }
+ */
 
 int main()
 {
-        //print_intro_screen();
-        run_event_loop();
+    //print_intro_screen();
+    run_event_loop();
 }
