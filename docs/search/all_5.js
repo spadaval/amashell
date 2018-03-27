8d5a25d4bf8425f78f261ee90f33b2810bc9f250
Sumeet Padavala <sumeet.padavala@gmail.com>
2018-03-27 17:24:39 +0530

    update docs

--- docs/search/all_5.js
+++ docs/search/all_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['newexecutable',['newExecutable',['../amash_8h.html#a01876584b372b9d39c167a30292385da',1,'newExecutable():&#160;structs.c'],['../structs_8c.html#a01876584b372b9d39c167a30292385da',1,'newExecutable():&#160;structs.c']]]
+  ['new_5fexecutable',['new_executable',['../amash_8h.html#ac87a118af55679693aed549766833324',1,'new_executable():&#160;structs.c'],['../structs_8c.html#ac87a118af55679693aed549766833324',1,'new_executable():&#160;structs.c']]]
 ];
