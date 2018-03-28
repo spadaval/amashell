dac3d2dee02f9c28d658148e009b7818af0a5ace
Sumeet Padavala <sumeet.padavala@gmail.com>
2018-03-28 16:37:26 +0530

    Update docs

--- docs/search/all_11.js
+++ docs/search/all_11.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['the_20amadeus_20shell',['The Amadeus Shell',['../index.html',1,'']]]
+  ['the_20amadeus_20shell',['The Amadeus Shell',['../index.html',1,'']]],
+  ['todo_20list',['Todo List',['../todo.html',1,'']]]
 ];
--- docs/search/functions_3.js
+++ docs/search/functions_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['handle_5fbuiltins',['handle_builtins',['../amash_8h.html#ab5335f75a5926e7cbbadc5b890f8678b',1,'handle_builtins(Executable *e) void do_cd(Executable *e):&#160;exec.c'],['../exec_8c.html#aa7406b94568c840be6913bdb0d3096c3',1,'handle_builtins(Executable *e):&#160;exec.c']]]
+  ['handle_5fbuiltins',['handle_builtins',['../amash_8h.html#aa7406b94568c840be6913bdb0d3096c3',1,'handle_builtins(Executable *e):&#160;exec.c'],['../exec_8c.html#aa7406b94568c840be6913bdb0d3096c3',1,'handle_builtins(Executable *e):&#160;exec.c']]]
 ];
