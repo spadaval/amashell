9e56f7fad47eaea4ce7b641c2c2b03481d2522f4
Sumeet Padavala <sumeet.padavala@gmail.com>
2018-03-31 20:11:54 +0530

    Add exec logic

--- src/exec.c
+++ src/exec.c
@@ -76,10 +76,58 @@ void exec_program(Executable *e)
 }
 
 /// \todo implement the exec function
-void exec(ParsedInput *i)
+void exec(ParsedInput *input)
 {
-    assert(false);
-    assert(i->executables_count == 1);
+    int pipe_read_fd = -1, pipe_write_fd = -1;
+    int fd[2];
+    for(int i = 0; i < input->executables_count; i++){
+        // set current executable as a local convenience variable
+        Executable* e = &input->executables[i];
+        if(i != input->executables_count - 1){
+            //set read head to the previously opened pipe
+            pipe_read_fd = fd[0];
+            //open a new pipe to send data to the next executable
+            pipe(fd);
+            //set write head to the currently opened pipe
+            pipe_write_fd = fd[1];
+        }
+
+        if (handle_builtins(e))
+        {
+            log_trace("Processed as builtin");
+        }
+        else
+        {
+            // fork and handle
+            log_trace("\tBeginning forked process");
+
+            if (fork() == 0)
+            {
+                /*child*/
+
+                set_redirects(e);
+                //if not the first executable
+                if(i != 0) {
+                    dup2(pipe_read_fd, fileno(stdin));
+                }
+                // if not the last executable
+                if(i != input->executables_count - 1){
+                    dup2(pipe_write_fd, fileno(stdout));
+                }
+
+                dump_executable(e);
+                if (execvp(e->exec_path, e->argv) != 0)
+                {
+                    log_error("Exec error(error %d)", errno);
+                    log_debug("exec_path:'%s'(%d)", e->exec_path, strlen(e->exec_path));
+                    printf(KNRM "\namash: command not found : '%s'\n\n", e->exec_path);
+                    abort();
+                }
+            }
+            wait(NULL);
+        }
+
+    }
 }
 
 void do_cd(Executable *e)
