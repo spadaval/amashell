311a403ef84959f949f8fcae37eb9983d2d66d29
Sumeet Padavala <sumeet.padavala@gmail.com>
2018-04-21 15:33:32 +0530

    Add alias definitions

--- src/alias.c
+++ src/alias.c
@@ -1,17 +1,59 @@
 /*!
-   \file alias.c
-   \brief Contains subroutines for setting, getting, and processing aliases.
+ * \file alias.c
+ * \brief Contains subroutines for setting, getting, and processing aliases.
  */
 #include "amash.h"
 
-char *get_alias(char *key)
+char* get_alias(char* key)
 {
+    for (int i = 0; i < MAX_ALIASES; i++)
+    {
+        if (!aliases->is_set[i])
+        {
+            continue;
+        }
+
+        if (strcmp(key, aliases->keys[i]) == 0)
+        {
+            return aliases->values[i];
+        }
+    }
+    return NULL;
 }
 
-void set_alias(char *key, char *value)
+
+void set_alias(char* key, char* value)
 {
+    int pos = aliases->insert_position;
+
+    aliases->keys[pos]       = key;
+    aliases->values[pos]     = value;
+    aliases->is_set[pos]     = true;
+    aliases->insert_position = (aliases->insert_position + 1) % MAX_ALIASES;
 }
 
-char *resolve_input(char *input)
+
+// TODO make this not crap
+char* resolve_input(char* input)
 {
+    log_debug("Resolving input string: '%s'", input);
+    char* output = input;
+
+    for (int i = 0; i < MAX_ALIASES; i++)
+    {
+        if (!aliases->is_set[i])
+        {
+            continue;
+        }
+
+        if (starts_with(aliases->keys[i], input))
+        {
+            output = calloc(sizeof(char), strlen(input) + strlen(aliases->values[i]));
+            strcpy(output, aliases->values[i]);
+
+            strcat(output, input + 1 + strlen(aliases->keys[i]));
+        }
+    }
+    log_debug("Resolved to: '%s'", output);
+    return output;
 }
