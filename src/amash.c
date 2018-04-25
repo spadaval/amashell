7e940196ce7bef965f89891d8cd9ef39b318d175
Sumeet Padavala <sumeet.padavala@gmail.com>
2018-04-26 00:19:34 +0530

    Remove continuation support

--- src/amash.c
+++ src/amash.c
@@ -13,38 +13,38 @@
  */
 int run_event_loop()
 {
-        while (true)
+    while (true)
+    {
+        printf("\n");
+        generatePrompt();
+        char* input = readline(prompt);
+
+        log_debug("Read input '%s'", input);
+
+        if (strlen(input) == 0)
         {
-                printf("\n");
-                generatePrompt();
-                char* input = readline(prompt);
-
-                log_debug("Read input '%s'", input);
-
-                if (strlen(input)==0)
-                {
-                        do_quit(NULL);
-                }
-
-                push_history(input);
-
-                for (int i = 0; i < count_lines(input); i++)
-                {
-                        char* s = extract_line(input);
-                        if(offset_sc==0)
-                        {
-                                break;
-                        }
-                        log_debug("Running input:%s", s);
-                        run_input(s);
-                }
-                //run_input(input);
-                offset_sc = 0;
-
-                //ParsedInput *e = parse(input);
-                //exec(e);
+            do_quit(NULL);
         }
-        return 0;
+
+        push_history(input);
+
+        /*for (int i = 0; i < count_lines(input); i++)
+         * {
+         *      char* s = extract_line(input);
+         *      if(offset_sc==0)
+         *      {
+         *              break;
+         *      }
+         *      log_debug("Running input:%s", s);
+         *      run_input(s);
+         * }*/
+        run_input(input);
+        offset_sc = 0;
+
+        //ParsedInput *e = parse(input);
+        //exec(e);
+    }
+    return 0;
 }
 
 
@@ -55,18 +55,18 @@ int run_event_loop()
  */
 void initialize()
 {
-        log_set_level(LOG_TRACE);
-        aliases = new_pairlist();
-        int pid;
-        if ((pid = vfork()) == 0)
-        {
-                printf("\n\n\n");
-                execle("/usr/bin/figlet\0","figlet\0","Amash Shell v1.1\0",NULL);
-                sleep(1);
-                //system("figlet \"Amash Shell v1.1\"");
-                exit(0);
-        }
-        wait(NULL);
+    log_set_level(LOG_TRACE);
+    aliases = new_pairlist();
+    int pid;
+    if ((pid = vfork()) == 0)
+    {
+        printf("\n\n\n");
+        execle("/usr/bin/figlet\0", "figlet\0", "Amash Shell v1.1\0", NULL);
+        sleep(1);
+        //system("figlet \"Amash Shell v1.1\"");
+        exit(0);
+    }
+    wait(NULL);
 }
 
 
@@ -75,12 +75,12 @@ void initialize()
  */
 int main()
 {
-        initialize();
-        run_event_loop();
+    initialize();
+    run_event_loop();
 }
 
 
 void push_history(char* input)
 {
-        add_history(input);
+    add_history(input);
 }
